9fa70adefe1562d428bededd72c72b5f6815a281
Przemek Przybylkiewicz <przemek.przybylkiewicz@gmail.com>
2022-02-06 20:04:41 +0100

    The basis of the project

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("test")
+from selenium import webdriver
+import unittest
+
+
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# import time
+
+class TestWebBank(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.driver = webdriver.Chrome(executable_path=r'E:\Prog\DriverSE\chromedriver.exe')
+        self.base_url = 'http://automationpractice.com/index.php'
+
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
+    # ---------------------------------------------------------------
+
+    def test_summarry(self):
+        driver = self.driver
+        driver.get(self.base_url)
+
+
+# ---------------------------------------------------------
